d9ca23fbe6888dfe1a1bc5e09ed28eaf1d3a5d49
viefimov <trooper.wers@yandex.ru>
2024-02-09 20:17:13 +0300

    fix mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,20 @@
 const cardList = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function addCard(initialCard, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").setAttribute("src", initialCard.link);
-  cardElement.querySelector(".card__title").textContent = initialCard.name;
-  cardList.append(cardElement);
+  cardElement.querySelector(".card__image").setAttribute("src", cardData.link);
+  cardElement.querySelector(".card__image").setAttribute("alt", `${cardData.name} на фото`);
+  cardElement.querySelector(".card__title").textContent = cardData.name;
   const deleteButton = cardElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
 }
 
 function deleteCard(evt) {
   evt.target.parentElement.remove();
 }
+initialCards.forEach((function (card) {
+    cardList.append(createCard(card, deleteCard));
+}));
 
-for (i = 0; i < initialCards.length; i++) {
-  addCard(initialCards[i], deleteCard);
-}
